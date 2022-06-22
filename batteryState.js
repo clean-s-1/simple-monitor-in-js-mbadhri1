c1db0a41d11891c719f1d76fb131c9eefb9c9ead
Badhrinarayanan <badhrinarayanan.m@siemens.com>
2022-06-22 14:30:51 +0530

    updated

--- batteryState.js
+++ batteryState.js
@@ -28,7 +28,7 @@ class BatteryState {
 		let validParameterRange = true;
 		ranges?.forEach((range) => {
 			if (value >= range.min && value < range.max) {
-				validParameterRange = range.validRange;
+				validParameterRange = range.validRange || false;
 				BatteryState.printMessage(range.message);
 			}
 		});
